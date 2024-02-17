6892ffcdf696425c0be5e518afe2d58fe87bc106
Ogura Deko <fanta51@live.jp>
2024-02-18 03:09:47 +0900

    プロジェクト ファイルを追加します。

---
+++ WEBPtoJPG_ZippedConverter/Main.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WEBPtoJPG_ZippedConverter
+{
+	partial class Main
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.workFolderBrowseText = new System.Windows.Forms.Button();
+			this.workFolderPathText = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.importZipFileBrowseButton = new System.Windows.Forms.Button();
+			this.importZipFilePathText = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.exportZipFilePathText = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.exportZipFileBrowseButton = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.executeButton = new System.Windows.Forms.Button();
+			this.logText = new System.Windows.Forms.RichTextBox();
+			this.titleLabel = new System.Windows.Forms.Label();
+			this.versionLabel = new System.Windows.Forms.Label();
+			this.githubLinkLabel = new System.Windows.Forms.LinkLabel();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.workFolderBrowseText);
+			this.groupBox1.Controls.Add(this.workFolderPathText);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.importZipFileBrowseButton);
+			this.groupBox1.Controls.Add(this.importZipFilePathText);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 35);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(530, 142);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "変換元";
+			// 
+			// workFolderBrowseText
+			// 
+			this.workFolderBrowseText.Location = new System.Drawing.Point(494, 94);
+			this.workFolderBrowseText.Name = "workFolderBrowseText";
+			this.workFolderBrowseText.Size = new System.Drawing.Size(25, 23);
+			this.workFolderBrowseText.TabIndex = 5;
+			this.workFolderBrowseText.Text = "..";
+			this.workFolderBrowseText.UseVisualStyleBackColor = true;
+			this.workFolderBrowseText.Click += new System.EventHandler(this.workFolderBrowseText_Click);
+			// 
+			// workFolderPathText
+			// 
+			this.workFolderPathText.Location = new System.Drawing.Point(6, 94);
+			this.workFolderPathText.Name = "workFolderPathText";
+			this.workFolderPathText.Size = new System.Drawing.Size(482, 23);
+			this.workFolderPathText.TabIndex = 4;
+			this.toolTip1.SetToolTip(this.workFolderPathText, "一時的に展開する際に使用する作業フォルダ");
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 76);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 15);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "ワークフォルダ";
+			// 
+			// importZipFileBrowseButton
+			// 
+			this.importZipFileBrowseButton.Location = new System.Drawing.Point(494, 37);
+			this.importZipFileBrowseButton.Name = "importZipFileBrowseButton";
+			this.importZipFileBrowseButton.Size = new System.Drawing.Size(25, 23);
+			this.importZipFileBrowseButton.TabIndex = 2;
+			this.importZipFileBrowseButton.Text = "..";
+			this.importZipFileBrowseButton.UseVisualStyleBackColor = true;
+			this.importZipFileBrowseButton.Click += new System.EventHandler(this.importZipFileBrowseButton_Click);
+			// 
+			// importZipFilePathText
+			// 
+			this.importZipFilePathText.Location = new System.Drawing.Point(6, 37);
+			this.importZipFilePathText.Name = "importZipFilePathText";
+			this.importZipFilePathText.Size = new System.Drawing.Size(482, 23);
+			this.importZipFilePathText.TabIndex = 1;
+			this.toolTip1.SetToolTip(this.importZipFilePathText, "変換元ファイルが存在するZIPファイルを入力");
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "ZIPファイル";
+			// 
+			// exportZipFilePathText
+			// 
+			this.exportZipFilePathText.Location = new System.Drawing.Point(6, 37);
+			this.exportZipFilePathText.Name = "exportZipFilePathText";
+			this.exportZipFilePathText.Size = new System.Drawing.Size(482, 23);
+			this.exportZipFilePathText.TabIndex = 4;
+			this.toolTip1.SetToolTip(this.exportZipFilePathText, "変換元ファイルが存在するZIPファイルを入力");
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.exportZipFileBrowseButton);
+			this.groupBox2.Controls.Add(this.exportZipFilePathText);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Location = new System.Drawing.Point(12, 188);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(530, 78);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "変換先";
+			// 
+			// exportZipFileBrowseButton
+			// 
+			this.exportZipFileBrowseButton.Location = new System.Drawing.Point(494, 37);
+			this.exportZipFileBrowseButton.Name = "exportZipFileBrowseButton";
+			this.exportZipFileBrowseButton.Size = new System.Drawing.Size(25, 23);
+			this.exportZipFileBrowseButton.TabIndex = 5;
+			this.exportZipFileBrowseButton.Text = "..";
+			this.exportZipFileBrowseButton.UseVisualStyleBackColor = true;
+			this.exportZipFileBrowseButton.Click += new System.EventHandler(this.exportZipFileBrowseButton_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 19);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(58, 15);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "ZIPファイル";
+			// 
+			// executeButton
+			// 
+			this.executeButton.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold);
+			this.executeButton.Location = new System.Drawing.Point(12, 283);
+			this.executeButton.Name = "executeButton";
+			this.executeButton.Size = new System.Drawing.Size(530, 68);
+			this.executeButton.TabIndex = 2;
+			this.executeButton.Text = "変換！";
+			this.executeButton.UseVisualStyleBackColor = true;
+			this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+			// 
+			// logText
+			// 
+			this.logText.Location = new System.Drawing.Point(12, 375);
+			this.logText.Name = "logText";
+			this.logText.Size = new System.Drawing.Size(530, 71);
+			this.logText.TabIndex = 4;
+			this.logText.Text = "";
+			// 
+			// titleLabel
+			// 
+			this.titleLabel.AutoSize = true;
+			this.titleLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+			this.titleLabel.Location = new System.Drawing.Point(116, 6);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(323, 21);
+			this.titleLabel.TabIndex = 5;
+			this.titleLabel.Text = "ZIP圧縮されたwebpファイルをjpgに変換するツール";
+			// 
+			// versionLabel
+			// 
+			this.versionLabel.AutoSize = true;
+			this.versionLabel.Location = new System.Drawing.Point(441, 12);
+			this.versionLabel.Name = "versionLabel";
+			this.versionLabel.Size = new System.Drawing.Size(40, 15);
+			this.versionLabel.TabIndex = 6;
+			this.versionLabel.Text = "Ver.x.x";
+			// 
+			// githubLinkLabel
+			// 
+			this.githubLinkLabel.AutoSize = true;
+			this.githubLinkLabel.Location = new System.Drawing.Point(506, 6);
+			this.githubLinkLabel.Name = "githubLinkLabel";
+			this.githubLinkLabel.Size = new System.Drawing.Size(45, 15);
+			this.githubLinkLabel.TabIndex = 7;
+			this.githubLinkLabel.TabStop = true;
+			this.githubLinkLabel.Text = "GitHub";
+			this.githubLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.githubLinkLabel_LinkClicked);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// Main
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(554, 457);
+			this.Controls.Add(this.githubLinkLabel);
+			this.Controls.Add(this.versionLabel);
+			this.Controls.Add(this.titleLabel);
+			this.Controls.Add(this.logText);
+			this.Controls.Add(this.executeButton);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Main";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Main_Load);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.AddItem_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.AddItem_DragEnter);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private GroupBox groupBox1;
+		private Button importZipFileBrowseButton;
+		private TextBox importZipFilePathText;
+		private Label label1;
+		private Label label2;
+		private Button workFolderBrowseText;
+		private TextBox workFolderPathText;
+		private ToolTip toolTip1;
+		private GroupBox groupBox2;
+		private Button exportZipFileBrowseButton;
+		private TextBox exportZipFilePathText;
+		private Label label3;
+		private Button executeButton;
+		private Label titleLabel;
+		private Label versionLabel;
+		private LinkLabel githubLinkLabel;
+		private OpenFileDialog openFileDialog1;
+		private FolderBrowserDialog folderBrowserDialog1;
+		private SaveFileDialog saveFileDialog1;
+		public RichTextBox logText;
+	}
+}
---
+++ WEBPtoJPG_ZippedConverter/Main.cs
@@ -0,0 +1,396 @@
+using System.Drawing.Imaging;
+using System.IO.Compression;
+using System.Text;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+
+namespace WEBPtoJPG_ZippedConverter
+{
+	public partial class Main : Form
+	{
+		private readonly string AppName = "WebpToJpg ZippedConverter";
+		private string randomKey = string.Empty;
+		public string message = string.Empty;
+
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void Main_Load(object sender, EventArgs e)
+		{
+			logText.Clear();
+			logText.AppendText(AppName + " Ver." + ProductVersion);
+			versionLabel.Text = "Ver." + ProductVersion;
+
+			// ランダムワークキー作成
+			var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".EnumerateRunes().ToArray();
+			var builder = new System.Text.StringBuilder();
+			Random r = new Random();
+			for (int i = 0; i < 5; i++)
+			{
+				builder.Append(chars[r.Next(chars.Length)]);
+			}
+			randomKey = builder.ToString();
+
+			workFolderPathText.Text = Directory.GetCurrentDirectory() + "\\" + randomKey + "\\";
+		}
+
+		private void importZipFileBrowseButton_Click(object sender, EventArgs e)
+		{
+			openFileDialog1 = new OpenFileDialog();
+			openFileDialog1.Title = "変換元ZIPファイルを選択";
+			openFileDialog1.Filter = "ZIPファイル(*.zip)|*.zip";
+			openFileDialog1.FileName = "";
+			if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+			{
+				importZipFilePathText.Text = openFileDialog1.FileName;
+				autoComplete(openFileDialog1.FileName);
+			}
+			return;
+		}
+
+		private void workFolderBrowseText_Click(object sender, EventArgs e)
+		{
+			folderBrowserDialog1 = new FolderBrowserDialog();
+			folderBrowserDialog1.Description = "作業フォルダを選択";
+			if (folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+			{
+				workFolderPathText.Text = ((folderBrowserDialog1.SelectedPath).EndsWith("\\") ? folderBrowserDialog1.SelectedPath : folderBrowserDialog1.SelectedPath + "\\") + randomKey + "\\";
+			}
+			return;
+		}
+
+		private void exportZipFileBrowseButton_Click(object sender, EventArgs e)
+		{
+			saveFileDialog1 = new SaveFileDialog();
+			saveFileDialog1.Title = "変換後ZIPファイル保存先を選択";
+			saveFileDialog1.Filter = "ZIPファイル(*.zip)|*.zip";
+			saveFileDialog1.FileName = "";
+			if (saveFileDialog1.ShowDialog() != DialogResult.Cancel)
+			{
+				exportZipFilePathText.Text = saveFileDialog1.FileName;
+			}
+			return;
+		}
+
+		private void executeButton_Click(object sender, EventArgs e)
+		{
+			if (importZipFilePathText.Text.Trim().Length == 0)
+			{
+				MessageBox.Show("参照元ZIPファイルは必須です。", AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				importZipFilePathText.Focus();
+				return;
+			}
+			if (workFolderPathText.Text.Trim().Length == 0)
+			{
+				MessageBox.Show("ワークフォルダは必須です。", AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				workFolderPathText.Focus();
+				return;
+			}
+			if (exportZipFilePathText.Text.Trim().Length == 0)
+			{
+				MessageBox.Show("変換後ZIPファイルは必須です。", AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				exportZipFilePathText.Focus();
+				return;
+			}
+			if (File.Exists(exportZipFilePathText.Text.Trim()))
+			{
+				MessageBox.Show("変換後ZIPファイルパスは既に存在しています。\n存在しないファイル名を指定してください。", AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				exportZipFilePathText.Focus();
+				return;
+			}
+			if (!(Path.GetExtension(importZipFilePathText.Text.Trim()).ToUpper() == ".ZIP"))
+			{
+				MessageBox.Show("参照元ZIPファイルパスはZIPファイルではありません。", AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				importZipFilePathText.Focus();
+				return;
+			}
+			if (!(Path.GetExtension(exportZipFilePathText.Text.Trim()).ToUpper() == ".ZIP"))
+			{
+				MessageBox.Show("変換後ZIPファイルパスはZIPファイルではありません。", AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				exportZipFilePathText.Focus();
+				return;
+			}
+
+			string importPath = importZipFilePathText.Text.Trim();
+			string workDir = workFolderPathText.Text.Trim();
+			string exportPath = exportZipFilePathText.Text.Trim();
+
+			logText.Clear();
+			logText.AppendText(AppName + " Ver." + ProductVersion);
+			addInfoMessage("処理開始！\n処理は別スレッドで実行されています。ダイアログが表示されるまでしばらくお待ち下さい。");
+
+			Thread t = new Thread(new ThreadStart(() =>
+			{
+				convert(importPath, workDir, exportPath);
+			}));
+			t.Start();
+
+			importZipFilePathText.Text = exportZipFilePathText.Text = string.Empty;
+		}
+
+		private void convert(string importPath, string workDirectory, string exportPath)
+		{
+			string baseFilePath = importPath;
+			string workDir = (workDirectory).EndsWith("\\") ? workDirectory.Trim() : workDirectory.Trim() + "\\";
+			string saveFilePath = exportPath;
+
+			// ワークディレクトリ内ファイルクリーン
+			addInfoMessage("ワークディレクトリ内ファイルクリーンを開始");
+			cleanWorkDir(workDir);
+
+			// ZIPファイル抽出
+			addInfoMessage("ZIPファイルの抽出を開始");
+
+			if (!UnZip(baseFilePath, workDir))
+			{
+				addInfoMessage("処理を中断しました。");
+				return;
+			}
+
+			// ファイル変換
+			addInfoMessage("ファイルの変換を開始");
+			if (!convertToJpg(workDir))
+			{
+				addInfoMessage("処理を中断しました。");
+				return;
+			}
+
+			// 再圧縮
+			addInfoMessage("ファイルの再圧縮を開始");
+			if (!compress(workDir + "convert", saveFilePath))
+			{
+				addInfoMessage("処理を中断しました。");
+				return;
+			}
+
+			// ワークディレクトリ内ファイルクリーン
+			addInfoMessage("ワークディレクトリ内ファイルクリーンを開始");
+			cleanWorkDir(workDir);
+
+			// 完了処理
+			addInfoMessage("完了！\n保存先：" + saveFilePath);
+			MessageBox.Show("処理完了！\n保存先\n[" + saveFilePath + "]", AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+			return;
+		}
+
+		private bool UnZip(string unZipFilePath, string workDir)
+		{
+			bool result = true;
+
+			// 展開元ファイル存在チェック
+			if (!File.Exists(unZipFilePath))
+			{
+				addErrorMessage("ファイルが見つかりません", unZipFilePath);
+				return false;
+			}
+
+			// 作業ディレクトリ存在チェック
+			if (!Directory.Exists(workDir))
+			{
+				// 存在しない場合、フォルダ作成
+				try
+				{
+					Directory.CreateDirectory(workDir);
+				}
+				catch (Exception ex)
+				{
+					addErrorMessage(ex.Message, workDir);
+					return false;
+				}
+			}
+
+			try
+			{
+				ZipFile.ExtractToDirectory(unZipFilePath, workDir);
+			}
+			catch (Exception ex)
+			{
+				addErrorMessage(ex.Message, unZipFilePath, workDir);
+				cleanWorkDir(workDir);
+				return false;
+			}
+			return result;
+		}
+
+		private bool convertToJpg(string workDir)
+		{
+			bool result = true;
+			if (File.Exists(workDir + "*.webp"))
+			{
+				addErrorMessage("webpファイルがありません", workDir);
+				cleanWorkDir(workDir);
+				return false;
+			}
+
+			// コンバートフォルダ作成
+			if (!Directory.Exists(workDir + "convert"))
+			{
+				try
+				{
+					Directory.CreateDirectory(workDir + "convert");
+				}
+				catch (Exception ex)
+				{
+					addErrorMessage(ex.Message, workDir + "convert");
+					cleanWorkDir(workDir);
+					return false;
+				}
+			}
+
+			// 変換処理
+			foreach (string file in Directory.GetFiles(workDir, "*.webp", SearchOption.TopDirectoryOnly))
+			{
+				string fileName = Path.GetFileNameWithoutExtension(file);
+				string outputPath = workDir + "convert\\" + fileName + ".jpg";
+
+				try
+				{
+					// 変換
+					Save(file, outputPath, ImageFormat.Jpeg);
+				}
+				catch (Exception ex)
+				{
+					addErrorMessage(ex.Message, file);
+					cleanWorkDir(workDir);
+					return false;
+				}
+			}
+			return result;
+		}
+
+		private bool compress(string workDir, string targetPath)
+		{
+			bool result = true;
+			try
+			{
+				ZipFile.CreateFromDirectory(workDir, targetPath);
+			}
+			catch (Exception ex)
+			{
+				addErrorMessage(ex.Message, workDir, targetPath);
+				cleanWorkDir(workDir);
+				return false;
+			}
+			return result;
+		}
+
+		public void addErrorMessage(string message, string path, string appendMessage = "")
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("\n[ERROR] ").Append(message).Append(" [").Append(path).Append("] ").Append(appendMessage).Append(" (").Append(DateTime.Now).Append(")");
+			MessageBox.Show(sb.ToString(), AppName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			/*
+			message = sb.ToString();
+
+			if (this.InvokeRequired)
+			{
+				this.Invoke(new Action(this.writeLog));
+			}
+			else
+			{
+				logText.AppendText(message);
+			}
+			*/
+			return;
+		}
+
+		public void addInfoMessage(string message)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("\n[INFO] ").Append(message).Append(" (").Append(DateTime.Now).Append(")");
+			message = sb.ToString();
+
+			if (this.InvokeRequired)
+			{
+				this.Invoke(new Action(this.writeLog));
+			}
+			else
+			{
+				logText.AppendText(message);
+			}
+			return;
+		}
+
+		private bool cleanWorkDir(string workDir)
+		{
+			bool result = true;
+			try
+			{
+				if (Directory.Exists(workDir))
+				{
+					Directory.Delete(workDir, true);
+				}
+			}
+			catch (Exception ex)
+			{
+				addErrorMessage(ex.Message, workDir);
+				return false;
+			}
+			return result;
+		}
+
+		private void writeLog()
+		{
+			logText.AppendText(message);
+			return;
+		}
+
+		private void Save(string inputFile, string outputFile, ImageFormat format)
+		{
+			var wf = new WebPFormat();
+			var stream = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+			var image = (Bitmap)wf.Load(stream);
+			image.Save(outputFile, format);
+			stream.Close();
+		}
+
+
+		private void autoComplete(string basePath)
+		{
+			string fullPath = ((Path.GetDirectoryName(basePath)).EndsWith("\\") ? Path.GetDirectoryName(basePath) : Path.GetDirectoryName(basePath) + "\\") + Path.GetFileNameWithoutExtension(basePath) + "_convert.zip";
+			exportZipFilePathText.Text = fullPath;
+		}
+
+		private void AddItem_DragDrop(object sender, DragEventArgs e)
+		{
+			// DataFormats.FileDropを与えて、GetDataPresent()メソッドを呼び出す。
+			var dropTarget = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+			// GetDataにより取得したString型の配列から要素を取り出す。
+			var targetFile = dropTarget[0];
+
+			importZipFilePathText.Text = targetFile;
+			autoComplete(targetFile);
+		}
+
+		private void AddItem_DragEnter(object sender, DragEventArgs e)
+		{
+			// マウスポインター形状変更
+			//
+			// DragDropEffects
+			//  Copy  :データがドロップ先にコピーされようとしている状態
+			//  Move  :データがドロップ先に移動されようとしている状態
+			//  Scroll:データによってドロップ先でスクロールが開始されようとしている状態、あるいは現在スクロール中である状態
+			//  All   :上の3つを組み合わせたもの
+			//  Link  :データのリンクがドロップ先に作成されようとしている状態
+			//  None  :いかなるデータもドロップ先が受け付けようとしない状態
+
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void githubLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			this.githubLinkLabel.LinkVisited = true;
+			System.Diagnostics.Process.Start("https://github.com/dekotan24/WEBPtoJPGZippedConverter");
+		}
+	}
+}
